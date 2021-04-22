31dd6b963488144abbcc45490ea5461c26ec53ea
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2021-04-22 10:02:04 +0300

    additions

--- chess.sql
+++ chess.sql
@@ -18,10 +18,10 @@ LANGUAGE plpgsql
 AS $$
     BEGIN 
 	    FOR i in 1..count loop
-		    INSERT INTO Chessman(class, color) values (chessman_class, 'white');
-			INSERT INTO Chessman(class, color) values (chessman_class, 'black');
-		END loop;
-	END
+                INSERT INTO Chessman(class, color) values (chessman_class, 'white');
+                INSERT INTO Chessman(class, color) values (chessman_class, 'black');
+            END loop;
+    END
 $$
 
 CREATE OR REPLACE PROCEDURE initialize_figures ()
@@ -29,34 +29,35 @@ LANGUAGE plpgsql
 AS 
 $$
     BEGIN
-	    call insert_chessman('king', 1);
-		call insert_chessman('queen', 1);
-		call insert_chessman('rook', 2);
-		call insert_chessman('bishop', 2);
-		call insert_chessman('knight', 2);
-		call insert_chessman('pawn', 8);
-	END
+	call insert_chessman('king', 1);
+	call insert_chessman('queen', 1);
+	call insert_chessman('rook', 2);
+	call insert_chessman('bishop', 2);
+	call insert_chessman('knight', 2);
+	call insert_chessman('pawn', 8);
+    END
 $$
 
 CREATE OR REPLACE PROCEDURE place_chessman(required_class char(20), coord_y int, coord_x int, step int)
 LANGUAGE plpgsql
 AS $$
     DECLARE 
-	chessman_record int;
-	i int := coord_x;
-	x_positions char[] := array['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    chessman_record int;
+    i int := coord_x;
+    x_positions char[] := array['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 	BEGIN
-	    for chessman_record in
-		    SELECT id FROM chessman WHERE class = required_class AND color = 'white'
+	    FOR chessman_record in
+		SELECT id FROM chessman WHERE class = required_class AND color = 'white'
 		LOOP
 		    INSERT INTO Chessboard VALUES (chessman_record, x_positions[i], coord_y);
-			i := i + step;
+		    i := i + step;
 		END LOOP;
-		i := coord_x;
-		FOR chessman_record IN 
-		    SELECT id FROM chessman WHERE class = required_class AND color = 'black'
-			LOOP
-			    INSERT INTO Chessboard VALUES (chessman_record, x_positions[i], 9 - coord_y);
-			END LOOP;
+	    i := coord_x;
+	    FOR chessman_record IN 
+                SELECT id FROM chessman WHERE class = required_class AND color = 'black'
+		LOOP
+	            INSERT INTO Chessboard VALUES (chessman_record, x_positions[i], 9 - coord_y);
+                    i := i + step;
+		END LOOP;
 	END
 $$
